8cf676274f2dd55dc077d96d0f2e1e5204c38844
Nam <hainam4296@gmail.com>
2022-09-26 18:02:48 +0700

    add admin controller

---
+++ app/code/Practice/Blog/Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Practice\Blog\Controller\Adminhtml\Post;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
